99cdd19e4298542992f7a4bdb6843f974d58f89d
Rene Heino <r.heino@newminds.nl>
2009-01-13 12:13:39 +0100

    add restricts search results to documents originating in a particular country

--- lib/googlesearch.rb
+++ lib/googlesearch.rb
@@ -7,7 +7,7 @@ class GoogleSearch
   attr_reader :response
   
   def initialize(options, search_request = SearchRequest.new)
-    @response = SearchResponse.new search_request.get("http://www.google.com/search?&q=#{options[:q]}&client=google-csbe&output=xml&cx=#{options[:cx]}")
+    @response = SearchResponse.new search_request.get("http://www.google.com/search?&q=#{options[:q]}&client=google-csbe&output=xml&cx=#{options[:cx]}&cr=lang_#{options[:cr]}")
   end
 end
 
--- spec/google_search_spec.rb
+++ spec/google_search_spec.rb
@@ -4,11 +4,11 @@ describe GoogleSearch do
   it do
     request_mock = mock('request')
     request_mock.should_receive(:get).with(
-      'http://www.google.com/search?&q=adres&client=google-csbe&output=xml&cx=unique-cse-id'
+      'http://www.google.com/search?&q=adres&client=google-csbe&output=xml&cx=unique-cse-id&cr=lang_nl'
     ).and_return('wat_xml')
     
     SearchResponse.should_receive(:new).with('wat_xml').and_return('search-response-object')
-    search = GoogleSearch.new({:cx => 'unique-cse-id', :q => 'adres'}, request_mock)
+    search = GoogleSearch.new({:cx => 'unique-cse-id', :q => 'adres', :cr => 'nl'}, request_mock)
     search.response.should == 'search-response-object'
   end
 end
