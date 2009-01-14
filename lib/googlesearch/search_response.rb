6f74bb2603b330dde69b67d80a4a89b97a5dada8
Matthijs Langenberg <mlangenberg@gmail.com>
2009-01-14 15:59:12 +0100

    search_response.rb looks the same as search_result.rb

--- lib/googlesearch/search_response.rb
+++ lib/googlesearch/search_response.rb
@@ -1,26 +1,14 @@
 class SearchResponse
-  attr_reader :index_of_first_result, :index_of_last_result
+  attr_reader :total_server_time, :total_number_of_results, :results, :index_of_first_result, :index_of_last_result
   def initialize(xml)
     doc = Nokogiri::XML(xml)
-    @tm = doc.root.xpath('TM').text.to_f 
+    @total_server_time = doc.root.xpath('TM').text.to_f 
     @index_of_first_result = doc.root.xpath('RES/@SN').text.to_i
     @index_of_last_result = doc.root.xpath('RES/@EN').text.to_i
-    @m = doc.root.xpath('RES/M').text.to_i
+    @total_number_of_results = doc.root.xpath('RES/M').text.to_i
     @results = []
     doc.root.xpath('RES//R').each do |res_doc|
       @results << SearchResult.new(res_doc)
     end
-  end
-  
-  def total_server_time
-    @tm
-  end
-  
-  def total_number_of_results
-    @m
-  end
-  
-  def results
-    @results
   end
 end
