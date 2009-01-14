5ba920c14e8f55971560d756d9a3bf57917cfe50
Matthijs Langenberg <mlangenberg@gmail.com>
2009-01-14 11:41:00 +0100

    Removed some newlines

--- lib/googlesearch/search_result.rb
+++ lib/googlesearch/search_result.rb
@@ -2,9 +2,7 @@ class SearchResult
   attr_reader :url, :index, :title, :excerpt, :language, :mime_type
   def initialize(doc)
     @index = doc.attributes["N"].text.to_i
-    
     @mime_type = doc.attributes["MIME"] ? doc.attributes["MIME"].text : nil
-    
     @url = doc.xpath('U').text
     @title = doc.xpath('T').text
     @excerpt = doc.xpath('S').text
