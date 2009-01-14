615ce68e522c7283f1a41f0eade06e9f66968e9a
Matthijs Langenberg <mlangenberg@gmail.com>
2009-01-14 11:49:29 +0100

    Support for setting lr in query.

--- lib/googlesearch.rb
+++ lib/googlesearch.rb
@@ -10,7 +10,7 @@ class GoogleSearch
   attr_reader :response
   
   def initialize(options, search_request = SearchRequest.new)
-    @response = SearchResponse.new search_request.get("http://www.google.com/search?&q=#{options[:q]}&client=google-csbe&output=xml&cx=#{options[:cx]}&cr=lang_#{options[:cr]}")
+    @response = SearchResponse.new search_request.get("http://www.google.com/search?&q=#{options[:q]}&client=google-csbe&output=xml&cx=#{options[:cx]}&cr=lang_#{options[:cr]}&lr=lang_#{options[:lr]}")
   end
 end
 
--- spec/google_search_spec.rb
+++ spec/google_search_spec.rb
@@ -1,14 +1,14 @@
 require File.join(File.dirname(__FILE__), 'spec_helper')
 
 describe GoogleSearch do
-  it do
+  it "should be able to create a search query" do
     request_mock = mock('request')
     request_mock.should_receive(:get).with(
-      'http://www.google.com/search?&q=adres&client=google-csbe&output=xml&cx=unique-cse-id&cr=lang_nl'
+      'http://www.google.com/search?&q=adres&client=google-csbe&output=xml&cx=unique-cse-id&cr=lang_nl&lr=lang_nl'
     ).and_return('wat_xml')
     
     SearchResponse.should_receive(:new).with('wat_xml').and_return('search-response-object')
-    search = GoogleSearch.new({:cx => 'unique-cse-id', :q => 'adres', :cr => 'nl'}, request_mock)
+    search = GoogleSearch.new({:cx => 'unique-cse-id', :q => 'adres', :cr => 'nl', :lr => 'nl'}, request_mock)
     search.response.should == 'search-response-object'
   end
 end
