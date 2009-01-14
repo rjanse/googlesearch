93fd610eb12acdac4dd523d7362034e209e5ce31
Matthijs Langenberg <mlangenberg@gmail.com>
2009-01-14 11:11:16 +0100

    Small fix

--- lib/googlesearch/search_result.rb
+++ lib/googlesearch/search_result.rb
@@ -2,7 +2,7 @@ class SearchResult
   attr_reader :url, :index, :title, :excerpt
   def initialize(doc)
     @url = doc.xpath('U').text
-    @index = doc.attributes["N"].to_i
+    @index = doc.attributes["N"].text.to_i
     @title = doc.xpath('T').text
     @excerpt = doc.xpath('S').text
   end
