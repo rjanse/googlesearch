34f2766b1d448f384945d937c343315ffaef8502
Matthijs Langenberg <mlangenberg@gmail.com>
2009-01-17 12:35:50 +0100

    reorderd things a bit

--- spec/search_response_spec.rb
+++ spec/search_response_spec.rb
@@ -3,8 +3,8 @@ require File.join(File.dirname(__FILE__), 'spec_helper')
 describe SearchResponse do
   
   setup do 
-    example_xml = File.read(File.join(File.dirname(__FILE__), 'result_example.xml'))
-    @response = SearchResponse.new(example_xml)
+    @example_xml = File.read(File.join(File.dirname(__FILE__), 'result_example.xml'))
+    @response = SearchResponse.new(@example_xml, 5)
   end
   
   it "shoud be able to return total server time in seconds" do
@@ -23,19 +23,16 @@ describe SearchResponse do
     @response.index_of_last_result.should == 5
   end
   
-  it "should return 10 for the requested number of search results" do
+  it "should return 5 for the requested number of search results" do
+    @response.requested_number_of_search_results.should == 5
+  end
+  
+  it "should return 10 for the requested number of search result as default" do
+    @response = SearchResponse.new(@example_xml)
     @response.requested_number_of_search_results.should == 10
   end
   
   it "should be able to return an array of SearchResult objects" do
      @response.results.map { |r| r.class }.should == [SearchResult]*5
-  end
-end
-
-describe SearchResponse, "with a custom requested number of search results" do
-  it "should return 20 for the requested number of search results" do
-    example_xml = File.read(File.join(File.dirname(__FILE__), 'result_example.xml'))
-    @response = SearchResponse.new(example_xml, 20)
-    @response.requested_number_of_search_results.should == 20
   end
 end
