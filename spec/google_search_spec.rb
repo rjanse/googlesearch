c9de22d06d1883caa26bda22dc5ba180d06f7542
Matthijs Langenberg <mlangenberg@gmail.com>
2009-01-21 09:45:51 +0100

    Fixed specs

--- spec/google_search_spec.rb
+++ spec/google_search_spec.rb
@@ -7,7 +7,7 @@ describe GoogleSearch do
       'http://www.google.com/search?&q=adres&client=google-csbe&output=xml_no_dtd&cx=unique-cse-id&cr=lang_nl&lr=lang_nl&start=0&num=10'
     ).and_return('wat_xml')
     
-    SearchResponse.should_receive(:new).with('wat_xml', 10).and_return('search-response-object')
+    SearchResponse.should_receive(:new).with('wat_xml', 10, 0).and_return('search-response-object')
     search = GoogleSearch.new({:cx => 'unique-cse-id', :q => 'adres', :cr => 'nl', :lr => 'nl', :start => 0, :num => 10 }, request_mock)
     search.response.should == 'search-response-object'
   end
