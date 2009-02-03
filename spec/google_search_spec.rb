18f2b44c4fc260dca67a1ab3d49a5a69717358a0
Robin <r.janse@newminds.nl>
2009-02-03 14:41:49 +0100

    rspec tests 0 failures. search parameters changed

--- spec/google_search_spec.rb
+++ spec/google_search_spec.rb
@@ -3,12 +3,10 @@ require File.join(File.dirname(__FILE__), 'spec_helper')
 describe GoogleSearch do
   it "should be able to create a search query" do
     request_mock = mock('request')
-    request_mock.should_receive(:get).with(
-      'http://www.google.com/search?cx=unique-cse-id&cr=lang_nl&output=xml_no_dtd&q=adres&lr=lang_nl&client=google-csbe&start=0&num=10'
-    ).and_return('wat_xml')
+    request_mock.should_receive(:get).with('http://www.google.com/search?cx=unique-cse-id&q=adres&cr=countryNL&start=10&num=10&lr=lang_nl&output=xml_no_dtd').and_return('wat_xml')
     
-    SearchResponse.should_receive(:new).with('wat_xml', 10, 0).and_return('search-response-object')
-    search = GoogleSearch.new({:cx => 'unique-cse-id', :q => 'adres', :cr => 'nl', :lr => 'nl', :start => 0, :num => 10 }, request_mock)
+    SearchResponse.should_receive(:new).with('wat_xml').and_return('search-response-object')
+    search = GoogleSearch.new({:cx => 'unique-cse-id', :q => 'adres', :cr => 'nl', :lr => 'nl'}, request_mock)
     search.response.should == 'search-response-object'
   end
 end
